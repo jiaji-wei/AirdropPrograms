40fb7ff68e19743cadf269ec5f0554de8004f2df
jacob <jiajiwei76@gmail.com>
2025-06-03 14:47:42 +0800

    [feat] update server code

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,17 @@ import { ethers } from "ethers";
 // load merkle tree or create new one
 // const merkleTree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("config/rewardProgram1.json", "utf8")));
 
+const programName = "rewardProgram1"; // usde-week23
+const id = ethers.keccak256(ethers.toUtf8Bytes(programName));
+console.log("programName:", programName);
+console.log("id:", id);
+
+
+const programName2 = "usde-week24";
+const id2 = ethers.keccak256(ethers.toUtf8Bytes(programName2));
+console.log("programName2:", programName2);
+console.log("id2:", id2);
+
 const wl = JSON.parse(fs.readFileSync("config/wl.json", "utf8"));
 const validList = [];
 let totalAmount = 0;
@@ -16,24 +27,38 @@ for (const [address, amount] of wl.rewardProgram1) {
     console.log("invalid address:", address);
     continue;
   }
-  validList.push([address, amount]);
+  validList.push([id, address, amount]);
   totalAmount += amount;
 }
-const merkleTree = StandardMerkleTree.of(validList, ["address", "uint256"]);
 
-// save merkle tree to file
-fs.writeFileSync("config/rewardProgram1.json", JSON.stringify(merkleTree.dump()));
+const validList2 = [];
+let totalAmount2 = 0;
+for (const [address, amount] of wl.usdeWeek24) {
+  // check address is valid
+  if (!ethers.isAddress(address)) {
+    console.log("invalid address:", address);
+    continue;
+  }
+  validList2.push([id2, address, amount]);
+  totalAmount2 += amount;
+}
 
-// (2)
-const programName = "rewardProgram1";
-const id = ethers.keccak256(ethers.toUtf8Bytes(programName));
-console.log("programName:", programName);
-console.log("id:", id);
+const merkleTree = StandardMerkleTree.of(validList, ["bytes32", "address", "uint256"]);
+const merkleTree2 = StandardMerkleTree.of(validList2, ["bytes32", "address", "uint256"]);
+
 const root = merkleTree.root;
 console.log("root:", root);
 console.log("totalAmount:", totalAmount);
 console.log("rewardAddressNum:", validList.length);
 
+const root2 = merkleTree2.root;
+console.log("root2:", root2);
+console.log("totalAmount2:", totalAmount2);
+console.log("rewardAddressNum2:", validList2.length);
+
+// save merkle tree to file
+fs.writeFileSync("config/rewardProgram1.json", JSON.stringify(merkleTree.dump()));
+fs.writeFileSync("config/usdeWeek24.json", JSON.stringify(merkleTree2.dump()));
 const app = express();
 const PORT = process.env.PORT || 9192;
 
@@ -46,6 +71,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
+let mapping = {};
+mapping[id] = merkleTree;
+mapping[id2] = merkleTree2;
+
+// /proof?address=0x123&programId=0x123  get proof
+// /proofs?address=0x123  get all reward program and proofs
 app.get("/proof", (req, res) => {
   try {
     const { address, programId } = req.query;
@@ -65,10 +97,12 @@ app.get("/proof", (req, res) => {
     let proof = null;
     let rewardAmount = 0;
 
+    const merkleTree = mapping[programId];
+
     for (const [i, v] of merkleTree.entries()) {
-      if (v[0] === normalizedAddress) {
+      if (v[1] === normalizedAddress) {
         proof = merkleTree.getProof(i);
-        rewardAmount = v[1];
+        rewardAmount = v[2];
         break;
       }
     }
